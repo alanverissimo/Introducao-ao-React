de7e36e9c32d5288ce73ce96c30bf8682c1d8fd3
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-24 02:02:24 -0300

    second commit

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import TechItem from "./TechItem";
+
 class TechList extends Component {
     state = {
         newTech: "",
@@ -25,13 +27,18 @@ class TechList extends Component {
         });
     }
 
+    handleDelete = (tech) => {
+        this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+
+    }
+
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
             <h1>{this.state.newTech}</h1>
             <ul>
-                {this.state.techs.map(tech => <li key={tech}>{tech}</li> )}
-            </ul>
+                {this.state.techs.map(tech =>  <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
+                </ul>               
             <input type="text" onChange={this.handleInputChange}
              value={this.state.newTech}
                 />
