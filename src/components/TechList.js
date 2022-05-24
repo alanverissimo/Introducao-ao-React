11a755b156e630d8224718ef192b4513215b59e7
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-24 01:39:45 -0300

    second commit

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 class TechList extends Component {
     state = {
+        newTech: "",
         techs: [
             "Node.js",
             "ReactJS",
@@ -9,11 +10,33 @@ class TechList extends Component {
         ]
     };
 
+    handleInputChange = e => {
+        this.setState({ newTech: e.target.value });
+
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.setState({
+         techs: [... this.state.techs, this.state.newTech],
+        newTech: ""
+
+        });
+    }
+
     render() {
         return (
+            <form onSubmit={this.handleSubmit}>
+            <h1>{this.state.newTech}</h1>
             <ul>
-                {this.state.techs.map(tech =><li>{tech}</li> )}
+                {this.state.techs.map(tech => <li key={tech}>{tech}</li> )}
             </ul>
+            <input type="text" onChange={this.handleInputChange}
+             value={this.state.newTech}
+                />
+                <button type="submit">Enviar</button>
+            </ form>
         );
     }
 }
