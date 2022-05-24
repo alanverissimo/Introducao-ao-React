da4c7e149c965b0b78c649af23fa27fbb4f0107d
Alan Verissimo <alan_verissimo@hotmail.com>
2022-05-24 02:25:01 -0300

    second commit

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -3,49 +3,57 @@ import React, { Component } from "react";
 import TechItem from "./TechItem";
 
 class TechList extends Component {
-    state = {
-        newTech: "",
-        techs: [
-            "Node.js",
-            "ReactJS",
-            "React Native"
-        ]
-    };
+  state = {
+    newTech: "",
+    techs: []
+  };
 
-    handleInputChange = e => {
-        this.setState({ newTech: e.target.value });
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
 
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
     }
+  }
 
-    handleSubmit = e => {
-        e.preventDefault();
-
-        this.setState({
-         techs: [... this.state.techs, this.state.newTech],
-        newTech: ""
-
-        });
+  componentDidUpdate(_,prevState) {
+    if (prevState !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
     }
+  }
 
-    handleDelete = (tech) => {
-        this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
 
-    }
+  handleSubmit = e => {
+    e.preventDefault();
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-            <h1>{this.state.newTech}</h1>
-            <ul>
-                {this.state.techs.map(tech =>  <TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
-                </ul>               
-            <input type="text" onChange={this.handleInputChange}
-             value={this.state.newTech}
-                />
-                <button type="submit">Enviar</button>
-            </ form>
-        );
-    }
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: "", 
+    });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => < TechItem key={tech} tech={tech} onDelete={() => this.handleDelete(tech)} />)}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    ); 
+  }
 }
 
 export default TechList;
